8e5ed0a92ca8a91c4ef4b37f5db87ba8b0b59351
Kelly McGuffog <kellymcguffog@gmail.com>
2023-01-17 19:46:25 -0500

    commit tree dfe90ef8ed601eb1ff0f0df447c8f7041665b5eb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 12, 'M', 'brown', 'mean', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 5, 'F', 'red', 'nice', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 8, 'F', 'blue', 'crazy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 22, 'M', 'white', 'loud', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 54, 'M', 'orange', 'shy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 2, 'M', 'yellow', 'happy', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 71, 'F', 'green', 'mean', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 162, 'M', 'black', 'sad', 0);
